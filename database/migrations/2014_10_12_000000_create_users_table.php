19e24998538f458a98a2ddb688c391e7dc96ab6a
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-08-17 20:54:31 +0300

    setup2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first-name');
+            $table->string('second-name');
+            $table->string('last-name');
+            $table->string('username')->unique();
+            $table->string('profile');
+            $table->date('birthdate');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
