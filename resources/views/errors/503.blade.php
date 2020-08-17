7d26eedec3c80b2ebce4c00a740c704cfdc5c6b8
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-08-18 00:05:42 +0300

    R

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __($exception->getMessage() ?: 'we are into something amazing'))
+@section('code', 'PUNTLAND')
+@section('message', __($exception->getMessage() ?: 'Shahaadada waxbarasho waxaa leh arday kasta oo Soomaaliyeed oo imtixaan la yaqaan galay'))
