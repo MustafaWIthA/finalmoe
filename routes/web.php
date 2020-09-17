274f0209e34912674aef9e6f49e89e634e803a96
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-09-18 00:34:58 +0300

    a
    a

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('home');
 // });
-Auth::routes(['register' => false]);
 
 // Route::get('/', );
 Route::get('/', 'HomeController@index')->name('home');
