bdb6f1a34faee64bd82ac36b083ad47bab96ff90
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-09-20 10:41:45 +0300

    s

--- resources/views/livewire/status.blade.php
+++ resources/views/livewire/status.blade.php
@@ -3,7 +3,7 @@
         
         <div class="bg-red-100 overflow-hidden shadow rounded-lg">
           <h3 class="text-xl p-3 text-center leading-10 font-bold text-white bg-blue-500">
-             Project Type
+             Projects Type
           </h3>
           <div class="px-4 py-5 sm:p-6">
             <div class="flex items-center">
