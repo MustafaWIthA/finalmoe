e190b7c06542da8da23afad45cb08c059989cf64
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-09-20 10:02:02 +0300

    riix

--- resources/views/livewire/status.blade.php
+++ resources/views/livewire/status.blade.php
@@ -141,6 +141,53 @@
       }
   });
   </script>
+<script>
+  var ctx = document.getElementById('barchartRegions').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: [
+            @foreach ($RegionProjects as $RegionProject)
+                '{{$RegionProject->name}}',
+            @endforeach
+            ],
+          datasets: [{
+              label: '# of Projects',
+              
+              data: [ 
+                @foreach ($RegionProjects as $RegionProject)
+                  {{$RegionProject->projects_count}},
+                @endforeach
+              ],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                 
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+               
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  },
+                  // gridLines: {
+                  //     display:false
+                  // } 
+              }]
+          }
+      }
+  });
+  </script>
 
   <script >
      var ctx = document.getElementById('myChart2').getContext('2d');
@@ -182,91 +229,6 @@
                   
               }]
           }
-      }
-  });
-  </script>
-
-<script>
-new Chart(document.getElementById("bar-chart-grouped"), {
-    type: 'bar',
-    data: {
-      labels: ["1900", "1950", "1999", "2050"],
-      datasets: [
-        {
-          label: "Sector",
-          backgroundColor: "#3e95cd",
-          data: [
-            @foreach ($SectorProjects as $SectorProject)
-                  {{$SectorProject->projects_count}},
-           @endforeach
-          ]
-        }, {
-          label: "Satatus",
-          backgroundColor: "#8e5ea2",
-          data: [
-            @foreach ($StateProjects as $StateProject)
-                  {{$StateProject->projects_count}},
-                @endforeach
-          ]
-        },
-        {
-          label:"Type",
-          backgroundColor:"#445566",
-          data:[
-            @foreach ($TypeProjects as $TypeProject)
-                '{{$TypeProject->projects_count}}',
-            @endforeach
-          ]
-          
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Population growth (millions)'
-      }
-    }
-});
-</script>
-
-
-<script>
-  new Chart(document.getElementById("barchartRegions"), {
-      type: 'bar',
-      data: {
-        labels: [
-             @foreach ($RegionProjects as $RegionProject)
-                    {{$RegionProject->name}},
-             @endforeach
-        ],
-        datasets: [
-          {
-            label: "Sector",
-            backgroundColor: "#3e95cd",
-            data: [
-              @foreach ($RegionProjects as $RegionProject)
-                    {{$RegionProject->projects_count}},
-             @endforeach
-            ]
-          },
-          {
-            label:"Type",
-            backgroundColor:"#445566",
-            data:[
-              @foreach ($TypeProjects as $TypeProject)
-                  '{{$TypeProject->projects_count}}',
-              @endforeach
-            ]
-            
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Population growth (millions)'
-        }
       }
   });
   </script>
