d439bfe96ea517e7b90862de37340574c8f60426
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-10-05 10:22:22 +0300

    for human change

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -42,13 +42,13 @@
               <svg class="flex-shrink-0 mr-1.5 h-10 w-7 text-gray-500" fill="white" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
               </svg>
-              Start Date: {{($project->start_date)->diffForHumans()}}
+              Start Date: {{$project->start_date}}
             </div>
             <div class="mt-2 flex items-center text-sm leading-5 text-gray-300">
                 <svg class="flex-shrink-0 mr-1.5 h-10 w-7 text-gray-500" fill="white" viewBox="0 0 20 20">
                   <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
                 </svg>
-                End Date: {{($project->end_date)->diffForHumans()}}
+                End Date: {{$project->end_date}}
               </div>
           </div>
         </div>
