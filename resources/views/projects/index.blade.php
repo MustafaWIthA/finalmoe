18b55947260aa56d7a59e42db7eeab7383c63baa
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-10-05 03:46:42 +0300

    s

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,13 +10,16 @@ Projects Data
       @include('partials.error')
     </h3>
     <div>
+      @hasrole('SuperAdmin|Editor')
       <span class="shadow-sm rounded-md">
-          <a href="projects/create">
-        <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out">
-          Create new Project
-        </button>
-    </a>
-      </span>
+        <a href="projects/create">
+          <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out">
+            Create new Project
+          </button>
+        </a>
+    </span>
+      @endhasrole
+      
     </div>
   </div>
   <br>
