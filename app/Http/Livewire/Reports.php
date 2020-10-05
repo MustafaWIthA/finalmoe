29b3d4812bd40c6b41893f575bc2f5a292b9dece
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-10-05 15:48:29 +0300

    go ahead

--- app/Http/Livewire/Reports.php
+++ app/Http/Livewire/Reports.php
@@ -4,7 +4,6 @@ namespace App\Http\Livewire;
 
 // use App\Type;
 
-use App\District;
 use App\Priority;
 use App\Project;
 use App\Region;
@@ -16,36 +15,24 @@ use Livewire\Component;
 class Reports extends Component
 
 {
-    public $type_id;
-    public $state_id;
-    public $state_ids=90;
-
-    public $region_id=1;
-    public $district_id=1;
+    public $type_id=1;
+    public $state_id=1;
     public $proirity_id=1;
-    public $sector_id=1;
-
-    public $status;
-    public $types;
-    public $projects;
+    public $region_id=1;
     
-  
-    public function change(){
 
-       $state_ids= $this->state_id;
-         $type_ids= $this->type_id;
-    }
 
-  
+    public $mystate=1;
+   
 
     public function render()
     {
+
+
         $r= Region::findOrfail($this->region_id);
         $p=Priority::findOrfail($this->proirity_id);
-        $s=Sector::findOrfail($this->sector_id);
+        $s=Sector::findOrfail(1);
 
-        $p=District::findOrfail($this->district_id);
-            
             $selectedprojects = Project::where('state_id', $this->state_id)
             ->where('type_id', $this->type_id)
             ->whereHas('regions', function($q) use ($r){
@@ -56,35 +43,11 @@ class Reports extends Component
                         $q->whereIn('sector_id', $s );})
             ->get();
             
-            $this->projects = $selectedprojects;
-        return view('livewire.reports');
+           // $this->projects = $selectedprojects;
+      
+
+        return view('livewire.reports',[
+            'selectedprojects'=>$selectedprojects,
+        ]);
     }
-
-
-
-    // public function mount()
-    // {
-    //     $r= Region::findOrfail(1);
-    //     $p=Priority::findOrfail(1);
-    //     $s=Sector::findOrfail(1);
-            
-    //         $state_id= $this->state_id;
-    //         $type_id= $this->type_id;
-
-
-    //         $selectedprojects = Project::where('state_id', $state_id)
-    //         ->where('type_id', $type_id)
-    //         ->whereHas('regions', function($q) use ($r){
-    //                     $q->whereIn('region_id', $r);})
-    //         ->WhereHas('priorities',function ($q) use ($p){
-    //                     $q->whereIn('priority_id', $p );})
-    //         ->WhereHas('sectors',function ($q) use ($s){
-    //                     $q->whereIn('sector_id', $s );})
-    //         ->get();
-            
-    //         $this->projects = $selectedprojects;
-    
-
-  
-    // }
 }
--- resources/views/livewire/reports.blade.php
+++ resources/views/livewire/reports.blade.php
@@ -1,70 +1,73 @@
-<div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-   <div class="sm:col-span-3">
-    <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Status</label>
-    <select wire:model="state_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-      <option>Select Status</option>
-      @foreach (App\State::all() as $state)
-    <option value="{{$state->id}}">{{$state->name}}</option>
-      @endforeach
-    </select>
-  </div>
+<div>
+  <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
     <div class="sm:col-span-3">
-      <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Type</label>
-      <select wire:model="type_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-        <option>select Type</option>
-        @foreach (App\Type::all() as $type)
-      <option value="{{$type->id}}">{{$type->name}}</option>
-        @endforeach
-      </select>
-    </div>
-    
-    <div class="sm:col-span-2">
-      <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Proirity</label>
-      <select wire:model="proirity_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-        <option>select Proirity</option>
-        @foreach (App\Priority::all() as $priority)
-      <option value="{{$priority->id}}">{{$priority->name}}</option>
-        @endforeach
-      </select>
-    </div>
-    <div class="sm:col-span-2">
-      <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Region</label>
-      <select wire:model="region_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-        <option>select Region</option>
-        @foreach (App\Region::all() as $region)
-      <option value="{{$region->id}}">{{$region->name}}</option>
-        @endforeach
-      </select>
-    </div>
-   
-    <div class="sm:col-span-2">
-      <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Districts</label>
-      <select wire:model="district_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-        <option>select Districts</option>
-        @foreach (App\District::all() as $district)
-      <option value="{{$district->id}}">{{$district->name}}</option>
-        @endforeach
-      </select>
-    </div>
-  
-    <div class="sm:col-span-6">
-      <button type="submit" class=" w-full justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out" 
-        wire:click="change">
-        Search
-      </button>
-    </div>
-    
+     <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Status</label>
+     <select wire:model="state_id" 
+     class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+       <option>Select Status</option>
+       @foreach (App\State::all() as $state)
+       <option value="{{$state->id}}">{{$state->name}}</option>
+       @endforeach
+   </select>
    </div>
-
-   {{$state_ids}}
-
+     <div class="sm:col-span-3">
+       <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Type</label>
+       <select wire:model="type_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+         <option>select Type</option>
+         @foreach (App\Type::all() as $type)
+       <option value="{{$type->id}}">{{$type->name}}</option>
+         @endforeach
+       </select>
+     </div>
+     
+     <div class="sm:col-span-2">
+       <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Proirity</label>
+       <select wire:model="proirity_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+         <option>select Proirity</option>
+         @foreach (App\Priority::all() as $priority)
+       <option value="{{$priority->id}}">{{$priority->name}}</option>
+         @endforeach
+       </select>
+     </div>
+     <div class="sm:col-span-2">
+       <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Region</label>
+       <select wire:model="region_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+         <option>select Region</option>
+         @foreach (App\Region::all() as $region)
+       <option value="{{$region->id}}">{{$region->name}}</option>
+         @endforeach
+       </select>
+     </div>
     
-    @forelse ($projects as $project)
-      <li> {{$project->title}} </li>
-    @empty
-      <p class="">Nothing back</p>
-    @endforelse
-
+     <div class="sm:col-span-2">
+       <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Districts</label>
+       <select wire:model="district_id" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+         <option>select Districts</option>
+         @foreach (App\District::all() as $district)
+       <option value="{{$district->id}}">{{$district->name}}</option>
+         @endforeach
+       </select>
+     </div>
+   
+     <div class="sm:col-span-6">
+       <button type="submit" class=" w-full justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out" 
+         wire:click="change">
+         Search
+       </button>
+     </div>
+     
+    </div>
+ 
+    {{$state_id}}
+ 
+     
+     @forelse ($selectedprojects as $project)
+       <li> {{$project->title}} </li>
+     @empty
+       <p class="">Nothing back</p>
+     @endforelse
+ 
+ </div>
 </div>
 
 
