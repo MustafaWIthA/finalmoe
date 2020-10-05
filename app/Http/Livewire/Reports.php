218a015a2af4c519ce510b90624dc79a693a28b4
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-10-05 20:24:21 +0300

    hy

--- app/Http/Livewire/Reports.php
+++ app/Http/Livewire/Reports.php
@@ -15,35 +15,34 @@ use Livewire\Component;
 class Reports extends Component
 
 {
-    public $type_id=1;
-    public $state_id=1;
-    public $proirity_id=1;
-    public $region_id=1;
+    public $state_id;
+    public $type_id;
+    public $proirity_id;
+    public $region_id;
     
 
-
-    public $mystate=1;
-   
+ 
 
     public function render()
     {
+        
 
+        // $r= Region::findOrfail($this->region_id);
+        // $p=Priority::findOrfail($this->proirity_id);
+        // $s=Sector::findOrfail(1);
+        
 
-        $r= Region::findOrfail($this->region_id);
-        $p=Priority::findOrfail($this->proirity_id);
-        $s=Sector::findOrfail(1);
-
+            
             $selectedprojects = Project::where('state_id', $this->state_id)
-            ->where('type_id', $this->type_id)
-            ->whereHas('regions', function($q) use ($r){
-                        $q->whereIn('region_id', $r);})
-            ->WhereHas('priorities',function ($q) use ($p){
-                        $q->whereIn('priority_id', $p );})
-            ->WhereHas('sectors',function ($q) use ($s){
-                        $q->whereIn('sector_id', $s );})
+                                ->where('type_id', $this->type_id)
+            // ->whereHas('regions', function($q) use ($r){
+            //             $q->whereIn('region_id', $r);})
+            // ->WhereHas('priorities',function ($q) use ($p){
+            //             $q->whereIn('priority_id', $p );})
+            // ->WhereHas('sectors',function ($q) use ($s){
+            //             $q->whereIn('sector_id', $s );})
             ->get();
             
-           // $this->projects = $selectedprojects;
       
 
         return view('livewire.reports',[
--- resources/views/livewire/reports.blade.php
+++ resources/views/livewire/reports.blade.php
@@ -3,7 +3,7 @@
     <div class="sm:col-span-3">
      <label for="location" class="block text-sm leading-5 font-medium text-gray-700">Status</label>
      <select wire:model="state_id" 
-     class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+      class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
        <option>Select Status</option>
        @foreach (App\State::all() as $state)
        <option value="{{$state->id}}">{{$state->name}}</option>
