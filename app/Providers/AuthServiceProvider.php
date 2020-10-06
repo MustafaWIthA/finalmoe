1d6d5dbafb6788b5991d55c4a42348fa71484328
MustafaWIthA <mustafa.abdsh@gmail.com>
2020-10-07 02:01:45 +0300

    n

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Policies\ProjectPolicy;
+use App\Policies\UserPolicy;
+use App\Project;
+use App\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +17,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+
+        User::class => UserPolicy::class,
+        Project::class => ProjectPolicy::class,
     ];
 
     /**
